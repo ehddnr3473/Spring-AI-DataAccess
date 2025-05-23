5fa6ceb20822fe2e0becd129a2c78539e8b8b8c4
김동욱 <ehddnr73@naver.com>
2025-05-23 15:54:30 +0900

    프로젝트 생성

---
+++ ai-dataaccess/src/main/java/com/yeolmok/aidataaccess/AiDataaccessApplication.java
@@ -0,0 +1,13 @@
+package com.yeolmok.aidataaccess;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiDataaccessApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AiDataaccessApplication.class, args);
+    }
+
+}
---
+++ ai-dataaccess/src/test/java/com/yeolmok/aidataaccess/AiDataaccessApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yeolmok.aidataaccess;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiDataaccessApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
